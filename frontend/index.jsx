cee5df7c75e4725fef953ea7e7264229d8fc34cb
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 15:29:33 -0800

    Create frontend entry file

---
+++ frontend/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDom.render(
+    <h1>Welcome to ZingLite</h1>,
+    document.getElementById('root'),
+  );
+});
