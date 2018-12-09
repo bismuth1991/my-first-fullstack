c43744651072d61deb402ad9bb11e2f394c0917a
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 21:35:59 -0800

    Create Greeting Component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import GreetingContainer from './greeting/GreetingContainer';
 
 const App = () => (
   <>
     <h1>ZingLite</h1>
+    <GreetingContainer />
   </>
 );
 
--- frontend/components/greeting/Greeting.jsx
+++ frontend/components/greeting/Greeting.jsx
@@ -1,10 +1,75 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Greeting = (
-  <>
-    <h2>Welcome to ZingLite</h2>
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
 
-  </>
-);
+    this.state = {
+      showDropdown: false,
+    };
+
+    this.showDropdown = this.showDropdown.bind(this);
+  }
+
+  showDropdown(e) {
+    e.preventDefault();
+
+    this.setState({
+      showDropdown: true,
+    });
+  }
+
+  hideDropdown(e) {
+    e.preventDefault();
+
+    this.setState({
+      showDropdown: false,
+    });
+  }
+
+  render() {
+    const guestGreeting = () => (
+      <>
+        <i
+          className="fa fa-user-o"
+          aria-hidden="true"
+          onMouseEnter={this.showDropdown}
+          onMouseLeave={this.hideDropdown}
+        />
+
+        <p>Welcome, Guest!</p>
+
+        <ul>
+          <li><Link to="/login">Log In</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
+      </>
+    );
+
+    const { currentUser, logout } = this.props;
+
+    const personalGreeting = () => (
+      <>
+        <i
+          className="fa fa-user-o"
+          aria-hidden="true"
+          onMouseEnter={this.showDropdown}
+          onMouseLeave={this.hideDropdown}
+        />
+
+        <p>
+          Hello,
+          {' '}
+          {currentUser.email}
+        </p>
+
+        <button type="button" onClick={logout}>Log Out</button>
+      </>
+    );
+
+    return currentUser ? personalGreeting() : guestGreeting();
+  }
+}
 
 export default Greeting;
