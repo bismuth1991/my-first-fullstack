7ccb20c2f47caf87c53c206c61fb2616be8a83ac
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 16:20:44 -0800

    Write out User model methods for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,38 @@
 class User < ApplicationRecord
-  
+
+  before_validation :ensure_session_token
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank"}
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return user if user && user.is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+
+    self.password_digest = Bcrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    Bcrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+
+    return self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
 end
