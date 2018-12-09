65d7fc040e10348d7d8c639b5384be24ddff953f
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 16:34:23 -0800

    Write helper methods #current_user, #login, and #logout! in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def current_user 
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+  end
+  
 end
