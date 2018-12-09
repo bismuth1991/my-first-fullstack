a58743d44e265dd044643eb34f4c006218b28826
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 18:07:19 -0800

    Write regular action creators, add receiveSessionErrors to thunk action creators

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,18 +2,39 @@ import * as SessionApiUtil from '../util/session_api_utils';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 
-export const login = user => (dispatch) => {
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const login = user => dispatch => (
   SessionApiUtil.login(user)
-    .then(dispatch(receiveCurrentUser(user)));
-};
+    .then(
+      currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveSessionErrors(errors)),
+    )
+);
 
-export const logout = () => (dispatch) => {
+export const logout = () => dispatch => (
   SessionApiUtil.logout()
-    .then(dispatch(logoutCurrentUser()));
-};
+    .then(() => dispatch(logoutCurrentUser()))
+);
 
-export const signup = user => (dispatch) => {
+export const signup = user => dispatch => (
   SessionApiUtil.signup(user)
-    .then(dispatch(receiveCurrentUser(user)));
-};
+    .then(
+      currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveSessionErrors(errors)),
+    )
+);
