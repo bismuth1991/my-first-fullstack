e5d9004282a15d5a9b8c9aac66845bdbefeea507
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 16:28:33 -0800

    Write out #create and #destroy for SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user 
+      login(@user)
+    else
+      render json: ["Invalid credentials"], status: 401
+    end
+  end 
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else 
+      render json: ["Nobody is signed in"], status: 400
+    end
+  end
+
 end
