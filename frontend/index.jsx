b4d2f26915f2d0def933a5c23fd95f671f9327c3
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 18:57:04 -0800

    Create App.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // TEST
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // ////
+
   ReactDom.render(
     <h1>Welcome to ZingLite</h1>,
     document.getElementById('root'),
