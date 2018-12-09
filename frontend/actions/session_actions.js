e18c4d2ee9e0c2aa1853386064deaed9da552d5d
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-08 17:51:08 -0800

    Write thunk action creators #login, #logout, and #signup

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,6 +1,19 @@
+import * as SessionApiUtil from '../util/session_api_utils';
+
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const login = user => (dispatch) => {
+  SessionApiUtil.login(user)
+    .then(dispatch(receiveCurrentUser(user)));
+};
 
+export const logout = () => (dispatch) => {
+  SessionApiUtil.logout()
+    .then(dispatch(logoutCurrentUser()));
+};
+
+export const signup = user => (dispatch) => {
+  SessionApiUtil.signup(user)
+    .then(dispatch(receiveCurrentUser(user)));
 };
