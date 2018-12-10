c5ff0bd25af401cc85a949d251d5c0e63d0fed56
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-10 14:50:57 -0800

    Create AuthRoute util to prevent user from login/signup when logged in and implement it in App

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Redirect } from 'react-router-dom';
+
+const Auth = ({
+  component: Component, path, loggedIn, exact,
+}) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props => (
+      !loggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    )}
+  />
+);
+
+const mapStateToProps = state => ({ loggedIn: Boolean(state.session.id) });
+
+const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+
+export default AuthRoute;
