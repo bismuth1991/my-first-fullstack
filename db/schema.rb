23ac4407225951f937b6fa78952e1af1316b8b7a
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-12 05:41:20 -0800

    Create Artist table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_000713) do
+ActiveRecord::Schema.define(version: 2018_12_12_133947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_artists_on_name"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title", null: false
