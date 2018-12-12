693f684bd85be8dd1b85a0c05aec37a451613f89
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-12 08:25:11 -0800

    Write receiveQueryAlbums/Artists action and coresponding thunk actions for search_bar

--- frontend/actions/search_bar_actions.js
+++ frontend/actions/search_bar_actions.js
@@ -9,7 +9,27 @@ const receiveQuerySongs = songs => ({
   songs,
 });
 
+const receiveQueryArtists = artists => ({
+  type: RECEIVE_QUERY_ARTISTS,
+  artists,
+});
+
+const receiveQueryAlbums = albums => ({
+  type: RECEIVE_QUERY_ALBUMS,
+  albums,
+});
+
 export const fetchQuerySongs = query => dispatch => (
   SearchBarApiUtil.fetchSongsByQuery(query)
     .then(songs => dispatch(receiveQuerySongs(songs)))
+);
+
+export const fetchQueryArtists = query => dispatch => (
+  SearchBarApiUtil.fetchArtistssByQuery(query)
+    .then(artists => dispatch(receiveQueryArtists(artists)))
+);
+
+export const fetchQueryAlbums = query => dispatch => (
+  SearchBarApiUtil.fetchAlbumsByQuery(query)
+    .then(albums => dispatch(receiveQueryAlbums(albums)))
 );
