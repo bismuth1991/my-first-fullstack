81c6bf12b9a6b774a0c604ca57ea37837b8925f8
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-18 08:54:05 -0800

    Create #show, #create, #patch, and #destroy actions for Playlist

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -5,18 +5,39 @@ class Api::PlaylistsController < ApplicationController
   end
 
   def show
-
+    render :show
   end
 
   def create
+    @playlist = Playlist.new(playlist_params)
 
+    if @playlist.save
+      render :show
+    else
+      render json: @playlist.errors.full_messagaes, status: 422
+    end
   end
 
   def patch
+    @playlist = Playlist.find(params[:id])
 
+    if @playlist.update(playlist_params)
+      render :show
+    else
+      render json: @playlist.errors.full_messagaes, status: 422
+    end
   end
 
   def destroy
+    @playlist = Playlist.find(params[:id])
+    @playlist.destroy
 
+    render :show
+  end
+
+  private 
+  
+  def playlist_params
+    params.require(:playlist).permit(:name)
   end
 end
