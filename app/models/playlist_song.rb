587171c9d975a2d79fcba07db461941fa739a1ae
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-18 09:09:08 -0800

    Validate PlaylistSongs model and create associations with Playlist and Song

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,2 +1,6 @@
 class PlaylistSong < ApplicationRecord
+  validates :song, uniqueness: { scope: :playlist, message: "Song already exists in playlist" }
+
+  belongs_to :playlist
+  belongs_to :song
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_161513) do
+ActiveRecord::Schema.define(version: 2018_12_18_170107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2018_12_18_161513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_artists_on_name"
+  end
+
+  create_table "playlist_songs", force: :cascade do |t|
+    t.integer "playlist_id", null: false
+    t.integer "song_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_id", "playlist_id"], name: "index_playlist_songs_on_song_id_and_playlist_id", unique: true
   end
 
   create_table "playlists", force: :cascade do |t|
