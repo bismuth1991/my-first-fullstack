8b9fbe3ab3f93919f01a11168528ac7bf6890b2b
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-18 10:33:28 -0800

    Write #create and #destroy actions for PlaylistSongsController

--- app/controllers/api/playlist_songs_controller.rb
+++ app/controllers/api/playlist_songs_controller.rb
@@ -1,9 +1,24 @@
 class Api::PlaylistSongsController < ApplicationController
   def create
+    @playlist_song = PlaylistSong.new(playlist_song_params)
 
+    if @playlist_song.save
+      render json: ["Song added to playlist"], status: 200
+    else
+      render json: @playlist_song.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @playlist_song = PlaylistSong.find(params[:id])
+    @playlist_song.destroy
 
+    render json: {}
+  end
+
+  private 
+   
+  def playlist_song_params
+    params.require(:playlist_song).permit(:playlist_id, :song_id)
   end
 end
