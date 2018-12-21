36b8a6a517f77739504226ebbebea5f4d5f589d8
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-20 23:53:35 -0800

    Move UserPlaylistShow connect to separate Container

--- frontend/components/playlist_show_page.jsx
+++ frontend/components/playlist_show_page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import UserPlaylistShow from './user_playlists/user_playlist_show';
+import UserPlaylistShowContainer from './user_playlists/user_playlist_show_container';
 import UserPlaylistContainer from './user_playlists/user_playlist_container';
 
 const PlaylistShowPage = props => (
   <>
     <UserPlaylistContainer />
-    <UserPlaylistShow {...props} />
+    <UserPlaylistShowContainer {...props} />
   </>
 );
 
--- frontend/components/user_playlists/user_playlist_show.jsx
+++ frontend/components/user_playlists/user_playlist_show.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import SongsContainer from '../songs_container';
 import UserPlaylistForm from './user_playlist_form';
-import { createPlaylist, editPlaylist, fetchUserPlaylist } from '../../actions/user_playlist_actions';
-import { removePlaylistSong } from '../../actions/playlist_songs_actions';
 
 class UserPlaylistShow extends React.Component {
   componentDidMount() {
@@ -23,43 +20,4 @@ class UserPlaylistShow extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const { match: { params: { playlistId } } } = ownProps;
-  const { entities: { songs } } = state;
-  const { session: { currentUser, playlistSongs, userPlaylists } } = state;
-
-  const playingSongIds = [];
-  const playingSongs = [];
-
-  for (let i = 0; i < Object.values(playlistSongs).length; i += 1) {
-    const song = songs[Object.values(playlistSongs)[i].songId];
-
-    playingSongs.push(song);
-    playingSongIds.push(song.id);
-  }
-
-  let playlistName;
-  if (Object.values(userPlaylists).length !== 0) {
-    playlistName = userPlaylists[playlistId].name;
-  }
-
-  return {
-    playlistId,
-    playlistName,
-    songs: playingSongs,
-    playingSongIds,
-    userId: currentUser.id,
-    playlistSongs: Object.values(playlistSongs),
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  savePlaylist: (name, userId, songIds) => dispatch(createPlaylist(name, userId, songIds)),
-  editPlaylist: (playlistId, name, userId, songIds) => dispatch(editPlaylist(playlistId, name, userId, songIds)),
-  fetchUserPlaylist: playlistId => dispatch(fetchUserPlaylist(playlistId)),
-  removePlaylistSong: playlistSongId => dispatch(removePlaylistSong(playlistSongId)),
-});
-
-export default connect(
-  mapStateToProps, mapDispatchToProps,
-)(UserPlaylistShow);
+export default UserPlaylistShow;
