745f5851a3956176d14f5a087d1444cb631edc18
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-20 23:02:02 -0800

    Clean up SongIndexContainer

--- frontend/components/songs_index/song_index_container.jsx
+++ frontend/components/songs_index/song_index_container.jsx
@@ -1,20 +1,12 @@
 import { connect } from 'react-redux';
-import { withCookies } from 'react-cookie';
-import { fetchSomeSongs, receiveSongs } from '../../actions/song_actions';
+import { fetchSomeSongs } from '../../actions/song_actions';
 import SongIndex from './song_index';
-import { playSong, addSongToList, addSongsToList } from '../../actions/audio_player_actions';
 
-const mapStateToProps = ({ entities: { songs }, session: { audioPlayer } }) => ({
+const mapStateToProps = ({ entities: { songs } }) => ({
   songs: Object.values(songs),
-  // playingSongs,
 });
 const mapDispatchtoProps = dispatch => ({
   fetchSomeSongs: offSet => dispatch(fetchSomeSongs(offSet)),
-  // receiveSongs: songs => dispatch(receiveSongs(songs)),
-  // playSong: (songId, playingSongId) => dispatch(playSong(songId, playingSongId)),
-  // addSongToList: songId => dispatch(addSongToList(songId)),
-  // addSongsToList: songIds => dispatch(addSongsToList(songIds)),
-  // addSongsToAudioPlayer: songIds => dispatch(addSongsToList(songIds)),
 });
 
 const SongIndexContainer = connect(
