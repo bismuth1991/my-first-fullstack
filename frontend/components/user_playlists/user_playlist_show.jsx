8904905a7aa1146a6d82bba03f6a1542d232f395
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-20 23:46:50 -0800

    Clean up PlaylistShowContainer

--- frontend/components/user_playlists/user_playlist_show.jsx
+++ frontend/components/user_playlists/user_playlist_show.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { withCookies } from 'react-cookie';
 import SongsContainer from '../songs_container';
 import UserPlaylistForm from './user_playlist_form';
-import { playSong, removeSongFromList, addSongsToList } from '../../actions/audio_player_actions';
 import { createPlaylist, editPlaylist, fetchUserPlaylist } from '../../actions/user_playlist_actions';
-import { receiveSongs } from '../../actions/song_actions';
 import { removePlaylistSong } from '../../actions/playlist_songs_actions';
 
 class UserPlaylistShow extends React.Component {
@@ -31,7 +28,6 @@ const mapStateToProps = (state, ownProps) => {
   const { entities: { songs } } = state;
   const { session: { currentUser, playlistSongs, userPlaylists } } = state;
 
-  // const playingSongTitles = [];
   const playingSongIds = [];
   const playingSongs = [];
 
@@ -39,7 +35,6 @@ const mapStateToProps = (state, ownProps) => {
     const song = songs[Object.values(playlistSongs)[i].songId];
 
     playingSongs.push(song);
-    // playingSongTitles.push(song.title);
     playingSongIds.push(song.id);
   }
 
@@ -52,7 +47,6 @@ const mapStateToProps = (state, ownProps) => {
     playlistId,
     playlistName,
     songs: playingSongs,
-    // playingSongTitles,
     playingSongIds,
     userId: currentUser.id,
     playlistSongs: Object.values(playlistSongs),
@@ -60,12 +54,8 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-  // receiveSongs: songs => dispatch(receiveSongs(songs)),
-  // playSong: (songId, playingSongIdx) => dispatch(playSong(songId, playingSongIdx)),
-  // removeSongFromList: songId => dispatch(removeSongFromList(songId)),
   savePlaylist: (name, userId, songIds) => dispatch(createPlaylist(name, userId, songIds)),
   editPlaylist: (playlistId, name, userId, songIds) => dispatch(editPlaylist(playlistId, name, userId, songIds)),
-  // addSongsToAudioPlayer: songIds => dispatch(addSongsToList(songIds)),
   fetchUserPlaylist: playlistId => dispatch(fetchUserPlaylist(playlistId)),
   removePlaylistSong: playlistSongId => dispatch(removePlaylistSong(playlistSongId)),
 });
